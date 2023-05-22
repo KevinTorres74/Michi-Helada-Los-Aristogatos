7e546c947cc63f0d2d61880ea4ca3b0eec3af0bc
DiegoPartida13 <78160536+DiegoPartida13@users.noreply.github.com>
2023-05-21 18:12:33 -0600

    "primer commit"

--- Michi-Helada/app.py
+++ Michi-Helada/app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+try:
+    conn = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='Starmoon66680',
+        database='ing_soft',
+        port=3306
+    )
+    print("Conexión exitosa a la base de datos")
+    conn.close()
+except pymysql.Error as e:
+    print("Error al conectar a la base de datos: ", e)
 
 @app.route('/')
 def hello_world():  # put application's code here
